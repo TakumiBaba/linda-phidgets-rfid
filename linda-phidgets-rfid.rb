9e04e6b632fac853d4fbf5e928401a77251267a8
TakumiBaba <s09704tb@gmail.com>
2013-11-07 13:55:42 +0900

    コードを短く

--- linda-phidgets-rfid.rb
+++ linda-phidgets-rfid.rb
@@ -1,32 +1,16 @@
 #!/usr/bin/env ruby
-require "rubygems"
-require "phidgets-ffi"
-require "eventmachine"
-require "em-rocketio-linda-client"
+Bundler.require
 
 EM::run do
   rfid = Phidgets::RFID.new
-
-  rfid.on_attach do |device, obj|
-    device.antenna = true
-    device.led = true
-    sleep 1
-  end
-
-  url = "http://linda.masuilab.org"
-  space = "delta"
-
-  linda = EM::RocketIO::Linda::Client.new url
-  ts = linda.tuplespace[space]
-
+  linda = EM::RocketIO::Linda::Client.new "http://linda.masuilab.org"
+  ts = linda.tuplespace["delta"]
   linda.io.on "connect" do
-    rfid.on_tag do |device, tag, obj|
-      puts "tag is #{tag}"
-      ts.write ["rfid", "on", tag]
+    rfid.on_tag do |device, id, obj|
+      ts.write ["rfid", "on", id]
     end
-    rfid.on_tag_lost do |device, tag, obj|
-      puts "tag lost #{tag}"
-      ts.write ["rfid", "off", tag]
+    rfid.on_tag_lost do |device, id, obj|
+      ts.write ["rfid", "off", id]
     end
   end
 end
